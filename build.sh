60cfb382b8cf51f560ba12f52a685bb03c1d6222
Supporterino <lars@roth-kl.de>
2021-05-13 11:21:20 +0000

    pilestacking

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@ cp -R www/* ../wizard_backend/client/
 
 cd /services/wizard_backend
 
-git add .
+#git add .
 
-git commit -m "Build ci image"
+#git commit -m "Build ci image"
 
-git push
+#git push
